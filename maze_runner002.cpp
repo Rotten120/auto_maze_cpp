1e79816d8d7c26a2f8480def7a8eb8eb9f24f0a9
Rotten120 <92904826+Rotten120@users.noreply.github.com>
2025-03-18 15:50:09 +0800

    code is formatted

--- maze_runner002.cpp
+++ maze_runner002.cpp
@@ -9,7 +9,7 @@ std::string board[MAZE_MAX_SIZE] = {
     "##.....###",
     "##.###.###",
     "##.#..E###",
-    "##.#.#.###",
+    "##.#######",
     "##.....###",
     "####.#####",
     "####S#####",
@@ -23,7 +23,7 @@ enum CELL_TYPE {
 
 bool orGate(bool arr[], const int size) {
     bool result = false;
-        
+
     for(int i = 0; i < size; i++) {
         result = result || arr[i];
     }
@@ -34,24 +34,33 @@ class Vec {
     int x;
     int y;
     public:
-    Vec(int x = 0, int y = 0): x(x), y(y) {}
-    void setVec(int _x, int _y) {setX(_x); setY(_y);}
+    Vec(int val = 0): x(val), y(val) {}
+    Vec(int x, int y): x(x), y(y) {}
     void setX(int _x) {x = _x;}
     void setY(int _y) {y = _y;}
     int getX() {return x;}
     int getY() {return y;}
-    Vec getSum(Vec vec2) {return Vec(x + vec2.getX(), y + vec2.getY());}
-    bool isEqual(Vec vec2) {return (x == vec2.getX() && y == vec2.getY());}
-    Vec getRev() {return Vec(-getX(), -getY());}
+    
+    Vec operator + (Vec vec2) {
+        return Vec(x + vec2.getX(), y + vec2.getY());
+    }
+    
+    bool operator == (Vec vec2) {
+        return (x == vec2.getX() && y == vec2.getY());
+    }
+    
+    Vec getRev() {
+        return Vec(x * -1, y * -1);
+    }
 };
 
 class Cell {
     CELL_TYPE type;
     bool isPathExists;
-    
+
     public:
     Cell(CELL_TYPE _type = NONE) : type(_type), isPathExists(false) {}
-    int getType() {return type;}
+    CELL_TYPE getType() {return type;}
     void setType(CELL_TYPE _type) {type = _type;}
     void setPath(bool _isPathExists) {isPathExists = _isPathExists;}
     bool isPath() {return isPathExists;}
@@ -61,7 +70,7 @@ class Cell {
             std::cout << '*';
             return;
         }
-        
+
         char typeIcon = ' ';
         switch(type) {
             case START: typeIcon = 'S'; break;
@@ -77,97 +86,91 @@ class Cell {
 class Maze {
     Vec size;
     Cell maze[MAZE_MAX_SIZE][MAZE_MAX_SIZE];
-    
+
     public:
     Maze(int x = MAZE_MAX_SIZE, int y = MAZE_MAX_SIZE, CELL_TYPE _type = WALL) {
         if (x > MAZE_MAX_SIZE || y > MAZE_MAX_SIZE) {
             std::cout << "Maze size is beyond MAZE_MAX_SIZE";
             return;
         }
-        size.setVec(x, y);
+        size = Vec(x, y);
         for(int i = 0; i < size.getY(); i++) 
             for(int j = 0; j < size.getX(); j++)
                 this->setCellType(Vec(j, i), _type);
     }
-    
+
     Cell getCell(Vec vec) {return maze[vec.getY()][vec.getX()];}
     int getSizeX() {return size.getX();}
     int getSizeY() {return size.getY();}
-    
+
     void setCellType(Vec vec, CELL_TYPE _type) {
         (&maze[vec.getY()][vec.getX()])->setType(_type);
     }
-    
+
     void setCellPath(Vec vec, bool _isPathExists) {
         (&maze[vec.getY()][vec.getX()])->setPath(_isPathExists);
     }
-    
+
     void print() {
         for(int i = 0; i < size.getY(); i++) {
             for(int j = 0; j < size.getX(); j++) {
-                this->getCell(Vec(j, i)).print();
+                getCell(Vec(j, i)).print();
                 if(j + 1 < size.getX()) std::cout << ' ';
             }
             std::cout << std::endl;
         }
     }
-    
+
     bool isOutofBounds(Vec pos) {
         return (!(
             pos.getX() > 0 || pos.getX() + 1 < getSizeX() ||
             pos.getY() > 0 || pos.getY() + 1 < getSizeY()
             ));
     }
-    
-    void printbool(bool arr[], const int size) {
-        for(int i = 0; i < size; i++)
-            std::cout << arr[i];
-        std::cout << std::endl;
-    }
-    
+
     bool findPathInit() {
-        Vec startPos();
-        Vec initDir();
-        
+        Vec startPos;
+        Vec initDir;
+
         for(int i = 0; i < size.getY(); i++) {
             for(int j = 0; j < size.getX(); j++) {
                 startPos = Vec(j, i);
-                if(getCell(startPos).getType == START) {
+                if(getCell(startPos).getType() == START) {
                     return findPath(initDir, startPos);
                 }
             }
         }
-        
+
         return false;
     }
-    
+
     bool findPath(Vec dir, Vec pos) {
-        CELL_TYPE posType = CELL_TYPE(getCell(pos).getType());
-        
+        CELL_TYPE posType = getCell(pos).getType();
+
         if(posType == END) return true;
         if (!(posType == PATH || posType == START)) return false;
-        
+
         const int dirs = 4;
         bool pathExists[4] = {false, false, false, false};
-        
+
         Vec delta[dirs] = {
                 Vec(-1, 0), Vec(1, 0),
                 Vec(0, -1), Vec(0, 1)
             };
-        
+
         for(int idx = 0; idx < dirs; idx++) {
-            Vec targetCellPos = pos.getSum(delta[idx]);
-            if(isOutofBounds(targetCellPos) || delta[idx].getRev().isEqual(dir))
+            Vec targetCellPos = pos + delta[idx];
+            if(isOutofBounds(targetCellPos) || (delta[idx].getRev() == dir))
                 continue;
-        
+
             Cell targetCell = getCell(targetCellPos);
             setCellPath(targetCellPos, true);
             pathExists[idx] = true;
-            
+
             if(!findPath(delta[idx], targetCellPos))
                 pathExists[idx] = false;
         }
-        
+
         bool anyPathsExists = orGate(pathExists, dirs);
         if (!anyPathsExists)
             setCellPath(pos, false);
@@ -193,7 +196,7 @@ void import(Maze& game, std::string b[]) {
 int main() {
     Maze game;
     import(game, board);
-    
+
     if(!game.findPathInit()) {
         std::cout << "UNSOLVABLE";
     } else {
